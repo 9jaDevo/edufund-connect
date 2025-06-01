7be5f659cc86350a3d075505bcb51b89c4a56df3
Akinwumi Michael <148793976+9jaDevo@users.noreply.github.com>
2025-06-01 20:26:32 +0100

    Update country detection service to use HTTPS

--- src/utils/countries.ts
+++ src/utils/countries.ts
@@ -50,7 +50,7 @@ export const useCountryDetection = () => {
   useEffect(() => {
     const detectCountry = async () => {
       try {
-        const response = await fetch('http://ip-api.com/json');
+        const response = await fetch('https://ip-api.com/json');
         
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
