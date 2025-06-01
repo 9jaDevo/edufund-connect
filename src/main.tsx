e4801cae058ddc820150c0dfe5fa957e7bcfb20a
Akinwumi Michael <148793976+9jaDevo@users.noreply.github.com>
2025-06-01 20:44:34 +0100

    Fix JSX file extension and imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { ReCaptchaProvider } from './utils/security';
+import { ReCaptchaProvider } from './utils/security.tsx';
 import './i18n/config';
 import './index.css';
 
